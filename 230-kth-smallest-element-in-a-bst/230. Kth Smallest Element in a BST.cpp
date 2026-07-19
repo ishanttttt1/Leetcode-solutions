63d4136389a7ef85a4e69cb0c8e1d4fb8f8c23d5
Ishant <ishantsharma1101@gmail.com>
2026-07-19 18:53:18 +0530

    Runtime: 0 ms (100.00%), Memory: 24.3 MB (90.65%) - Gitcode

--- 230-kth-smallest-element-in-a-bst/230. Kth Smallest Element in a BST.cpp
+++ 230-kth-smallest-element-in-a-bst/230. Kth Smallest Element in a BST.cpp
@@ -1,20 +1,20 @@
 class Solution{
 public:
-        int count = 0;
-        int ans = 0;
-        void inorder(TreeNode*root,int k){
-            if (root==nullptr)
-            return;
-            inorder(root->left,k);
-            count++;
-            if(count==k){
-            ans=root->val;
+int count=0;
+int ans=0;
+void Inorder(TreeNode*root,int k){
+        if(root==nullptr){
             return;
         }
-        inorder(root->right,k);
-    }
-    int kthSmallest(TreeNode* root, int k){
-        inorder(root,k);
-        return ans;
+        Inorder(root->left,k);
+        count++;
+        if(count==k){
+            ans=root->val;
+        }
+        Inorder(root->right,k);
 }
+    int kthSmallest(TreeNode* root, int k){
+        Inorder(root,k);
+        return ans;
+    }
 };
