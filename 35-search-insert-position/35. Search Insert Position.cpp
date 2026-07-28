2dbb16f161e8f921f6fdd128c4295cd17a55d881
Ishant <ishantsharma1101@gmail.com>
2026-07-28 17:53:03 +0530

    Runtime: 0 ms (100.00%), Memory: 13.7 MB (43.84%) - Gitcode

--- 35-search-insert-position/35. Search Insert Position.cpp
+++ 35-search-insert-position/35. Search Insert Position.cpp
@@ -1,19 +1,19 @@
- //upper bound use hoga 
- class Solution{
+class Solution{
 public:
     int searchInsert(vector<int>& nums, int target){
+        int ans=nums.size();
         int low=0;
         int high=nums.size()-1;
-        int ans =nums.size();
         while(low<=high){
             int mid=low+(high-low)/2;
             if(nums[mid]>=target){
                 ans=mid;
                 high=mid-1;
-            }else{
-                low=mid+1;
             }
-        }
+            else{
+                low=mid+1;
+                }
+                } 
         return ans;
     }
 };
