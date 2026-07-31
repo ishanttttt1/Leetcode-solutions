18b809127570f9cf7283b549be69fece6d121356
Ishant <ishantsharma1101@gmail.com>
2026-08-01 00:46:49 +0530

    Runtime: 0 ms (100.00%), Memory: 7.9 MB (23.44%) - Gitcode

---
+++ 633-sum-of-square-numbers/633. Sum of Square Numbers.cpp
@@ -0,0 +1,21 @@
+class Solution{
+public:
+    bool judgeSquareSum(int c){
+        int a=0;
+        int b=sqrt(c);
+        while(a<=b){
+            long long sum=(long long)a*a+(long long)b*b;
+            if(sum==c){
+                return true;
+            }
+            else if(sum<c){
+                a++;
+            }
+            else{
+                b--;
+            }
+        }
+return false;
+        
+    }
+};
