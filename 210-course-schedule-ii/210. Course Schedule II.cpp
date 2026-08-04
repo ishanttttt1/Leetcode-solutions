afd35f912daac21c0af61ad3a288435d6f08db01
Ishant <ishantsharma1101@gmail.com>
2026-08-05 00:05:28 +0530

    Runtime: 11 ms (11.56%), Memory: 18 MB (91.78%) - Gitcode

--- 210-course-schedule-ii/210. Course Schedule II.cpp
+++ 210-course-schedule-ii/210. Course Schedule II.cpp
@@ -1,41 +1,33 @@
-class Solution {
+class Solution{
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int>findOrder(int numCourses, vector<vector<int>>& prerequisites){
         vector<vector<int>> adj(numCourses);
-        vector<int> inDegree(numCourses, 0);
-        vector<int> courseOrder;
-        
-        // Build adjacency list and in-degree count
-        for (auto& pre : prerequisites) {
-            int course = pre[0];
-            int prerequisite = pre[1];
-            adj[prerequisite].push_back(course);
-            inDegree[course]++;
+vector<int>indegree(numCourses,0);
+for(auto &it:prerequisites){
+    adj[it[1]].push_back(it[0]);
+    indegree[it[0]]++;
+}
+queue<int>q;
+for(int i=0;i<numCourses;i++){
+    if(indegree[i]==0){
+        q.push(i);
+    }
+}
+    vector<int>ans;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        ans.push_back(node);
+        for(int neighbor:adj[node]){
+            indegree[neighbor]--;
+            if(indegree[neighbor]==0)
+            q.push(neighbor);
         }
+    }
+    if(ans.size()==numCourses)
+    return ans;
+    return {};
+
         
-        // Queue for courses with no prerequisites (in-degree = 0)
-        queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
-                q.push(i);
-            }
-        }
-        
-        // Process courses in topological order
-        while (!q.empty()) {
-            int course = q.front();
-            q.pop();
-            courseOrder.push_back(course);
-            
-            for (int nextCourse : adj[course]) {
-                inDegree[nextCourse]--;
-                if (inDegree[nextCourse] == 0) {
-                    q.push(nextCourse);
-                }
-            }
-        }
-        
-        // If all courses are processed, return order; otherwise, return empty array (cycle detected)
-        return (courseOrder.size() == numCourses) ? courseOrder : vector<int>();
     }
 };
