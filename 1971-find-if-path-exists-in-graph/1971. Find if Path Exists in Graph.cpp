cffe6d82027aa1469f6806ea8a582b86895e16c2
Ishant <ishantsharma1101@gmail.com>
2026-08-06 00:13:39 +0530

    Runtime: 648 ms (7.50%), Memory: 298.5 MB (64.04%) - Gitcode

--- 1971-find-if-path-exists-in-graph/1971. Find if Path Exists in Graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971. Find if Path Exists in Graph.cpp
@@ -1,29 +1,26 @@
 class Solution{
 public:
-bool dfs(int node,int destination,vector<vector<int>>&adj,vector<bool>&visited){
-    if(node==destination){
+bool dfs(int currentnode,int destination,vector<vector<int>>&graph,vector<bool>&visited){
+    if(visited[currentnode]){
+        return false;
+    }
+    if(currentnode==destination){
         return true;
     }
-    visited[node]=true;
-    for(int neighbor:adj[node]){
-        if(!visited[neighbor]){
-            if(dfs(neighbor,destination,adj,visited)){
-                return true;
-            }
-        }
+    visited[currentnode]=true;
+    for(int neighbor:graph[currentnode]){
+if(dfs(neighbor,destination,graph,visited))
+return true;
     }
     return false;
 }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination){
-        vector<vector<int>>adj(n);
-        for(auto&edge:edges){
-            int u=edge[0];
-            int v=edge[1];
-            adj[u].push_back(v);
-            adj[v].push_back(u);
-        }
+        vector<vector<int>>graph(n);
         vector<bool>visited(n,false);
-        return dfs(source,destination,adj,visited);
-        
+        for(auto &edges:edges){
+            graph[edges[0]].push_back(edges[1]);
+            graph[edges[1]].push_back(edges[0]);
+        }
+        return dfs(source,destination,graph,visited);
     }
 };
