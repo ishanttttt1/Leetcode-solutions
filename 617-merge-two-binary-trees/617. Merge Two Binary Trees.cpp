e11843dfb88604ed20e22667eadc9b191f35137a
Ishant <ishantsharma1101@gmail.com>
2026-08-15 09:51:57 +0530

    Runtime: 0 ms (100.00%), Memory: 33.1 MB (87.75%) - Gitcode

--- 617-merge-two-binary-trees/617. Merge Two Binary Trees.cpp
+++ 617-merge-two-binary-trees/617. Merge Two Binary Trees.cpp
@@ -10,9 +10,10 @@ public:
         if(root2==nullptr){
             return root1;
         }
-        root1->val+=root2->val;
+        root1->val=root1->val+root2->val;
         root1->left=mergeTrees(root1->left,root2->left);
         root1->right=mergeTrees(root1->right,root2->right);
         return root1;
+        
     }
 };
