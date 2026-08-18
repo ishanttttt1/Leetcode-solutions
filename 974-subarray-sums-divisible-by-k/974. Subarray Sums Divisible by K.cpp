e1be11b8be459c3c3c7b1caa0775d4061bfc5707
Ishant <ishantsharma1101@gmail.com>
2026-08-18 23:45:07 +0530

    Runtime: 13 ms (34.27%), Memory: 35.5 MB (90.80%) - Gitcode

--- 974-subarray-sums-divisible-by-k/974. Subarray Sums Divisible by K.cpp
+++ 974-subarray-sums-divisible-by-k/974. Subarray Sums Divisible by K.cpp
@@ -1,23 +1,21 @@
 class Solution{
 public:
     int subarraysDivByK(vector<int>& nums, int k){
-        unordered_map<int,int>mp;
-        int sum = 0;
-        int count = 0;
-        mp[0]=1;
-        for(int i = 0 ;i<nums.size();i++){
-            sum+=nums[i];
-            int remainder=sum%k;
-            if(remainder<0){
-            remainder+=k;
-        }
-        if(mp.find(remainder)!=mp.end()){
-            count+=mp[remainder];
-        }
-        mp[remainder]++;
-        }
-        return count;
-
-        
+    unordered_map<int,int>mp;
+    int sum=0;
+    int count=0;
+         mp[0]=1;
+for(int i=0;i<nums.size();i++){
+    sum=sum+nums[i];
+    int remainder=sum%k;
+    if(remainder<0){
+    remainder=remainder+k;
+    }
+    if(mp.find(remainder)!=mp.end()){
+        count=count+mp[remainder];
+    }
+    mp[remainder]++;
+}
+return count;
     }
 };
