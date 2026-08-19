dd3d64c34ce487ce2d634caeb08edbe1a8f53476
Ishant <ishantsharma1101@gmail.com>
2026-08-19 22:28:47 +0530

    Runtime: 0 ms (100.00%), Memory: 15.4 MB (36.75%) - Gitcode

--- 33-search-in-rotated-sorted-array/33. Search in Rotated Sorted Array.cpp
+++ 33-search-in-rotated-sorted-array/33. Search in Rotated Sorted Array.cpp
@@ -4,27 +4,25 @@ public:
         int low=0;
         int high=nums.size()-1;
         while(low<=high){
-          int  mid=low+(high-low)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }
-             if(nums[low]<=nums[mid]){
+            if(nums[low]<=nums[mid]){
                 if(target>=nums[low]&&target<nums[mid]){
                     high=mid-1;
                 }else{
                     low=mid+1;
                 }
-             }
-                else{
+            }
+                else{ 
                     if(target>nums[mid]&&target<=nums[high]){
-                        low=mid+1;
-                    }
-                    else{
-                        high=mid-1;
-                    }
-                }
-
-             }
- return -1;       
+                    low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        }
+        return -1;        
     }
 };
