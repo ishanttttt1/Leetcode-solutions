a263c052457adfd1dcf12113df876ae2fd45c521
Ishant <ishantsharma1101@gmail.com>
2026-08-20 18:31:50 +0530

    Runtime: 40 ms (41.43%), Memory: 72.5 MB (61.71%) - Gitcode

--- 215-kth-largest-element-in-an-array/215. Kth Largest Element in an Array.cpp
+++ 215-kth-largest-element-in-an-array/215. Kth Largest Element in an Array.cpp
@@ -1,6 +1,6 @@
 class Solution{
 public:
-    int findKthLargest(vector<int>& nums, int k){
+    int findKthLargest(vector<int>& nums, int k){    
         priority_queue<int,vector<int>,greater<int>>pq;
         for(int i=0;i<nums.size();i++){
             pq.push(nums[i]);
@@ -8,7 +8,6 @@ public:
                 pq.pop();
             }
         }
-        return  pq.top();
-        
-    }
+        return pq.top();
+        }
 };
