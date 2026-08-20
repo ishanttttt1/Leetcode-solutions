3be69dace73f117240750d17edbb8ed32a637c75
Ishant <ishantsharma1101@gmail.com>
2026-08-20 18:03:23 +0530

    Runtime: 39 ms (46.13%), Memory: 61.7 MB (100.00%) - Gitcode

--- 215-kth-largest-element-in-an-array/215. Kth Largest Element in an Array.cpp
+++ 215-kth-largest-element-in-an-array/215. Kth Largest Element in an Array.cpp
@@ -1,33 +1,14 @@
 class Solution{
 public:
-    int partition(vector<int>&nums,int low,int high){
-        int pivot=nums[high];
-        int i=low-1;
-        for(int j=low;j<high;j++){
-            if(nums[j]<pivot){
-                i++;
-                swap(nums[i],nums[j]);
+    int findKthLargest(vector<int>& nums, int k){
+        priority_queue<int,vector<int>,greater<int>>pq;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+            if(pq.size()>k){
+                pq.pop();
             }
         }
-        swap(nums[i+1],nums[high]);
-        return i+1;
+        return  pq.top();
+        
     }
- int findKthLargest(vector<int>& nums, int k){
-    
-    int n=nums.size();
-    int target=n-k;
-    int low=0;
-    int high=n-1;
-    while(low<=high){
-        int p =partition(nums,low,high);
-        if(p==target){
-            return nums[p];
-        }else if(p<target){
-            low=p+1;
-        }else{ 
-        high=p-1;
-        }
-    }   
-    return -1;
- }
 };
