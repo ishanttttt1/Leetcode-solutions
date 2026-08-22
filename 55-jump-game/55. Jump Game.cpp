93d8eaf13524606491ed1c4e254f4d10d8411c81
Ishant <ishantsharma1101@gmail.com>
2026-08-22 23:21:39 +0530

    Runtime: 670 ms (5.02%), Memory: 55.9 MB (5.91%) - Gitcode

--- 55-jump-game/55. Jump Game.cpp
+++ 55-jump-game/55. Jump Game.cpp
@@ -1,15 +1,23 @@
-class Solution {
+class Solution{
 public:
-    bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        int farthest = 0;  
-        
-        for (int i = 0; i < n; i++) {
-            if (i > farthest) return false;  
-            farthest = max(farthest, i + nums[i]);  
-            if (farthest >= n - 1) return true;  
-        }
-        
+bool solve(int i,vector<int>&nums,vector<int>&dp){
+    if(i==nums.size()-1){
         return true;
+    }
+    if(dp[i]!=-1){
+        return dp[i];
+    }
+    for(int jump=1;jump<=nums[i];jump++){
+        int next=i+jump;
+        if(next<nums.size()&&solve(next,nums,dp)){
+            return dp[i]=true;
+        }
+    }
+    return dp[i]=false;
+}
+    bool canJump(vector<int>& nums){
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return solve(0,nums,dp);
     }
 };
