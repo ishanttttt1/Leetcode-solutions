2ae6b622fdcc77c359ecb08feb21eebdb102a918
Ishant <ishantsharma1101@gmail.com>
2026-08-23 17:34:50 +0530

    Runtime: 19 ms (87.05%), Memory: 88.2 MB (93.39%) - Gitcode

---
+++ 525-contiguous-array/525. Contiguous Array.cpp
@@ -0,0 +1,20 @@
+class Solution{
+public:
+    int findMaxLength(vector<int>& nums){
+        unordered_map<int,int>mp;
+        int sum = 0;
+        int maxLen=0;
+        mp[0]=-1;
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i]==0)sum-=1;else sum+=1;
+        if(mp.find(sum)!=mp.end()){
+            int len = i-mp[sum];
+            maxLen=max(maxLen,len);
+
+        }else{
+            mp[sum]=i;
+        }
+        }
+        return maxLen;
+    }
+};
