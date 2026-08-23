d0e5bb9f5ffa5ecd63a0c62633c147247dbdf176
Ishant <ishantsharma1101@gmail.com>
2026-08-23 18:02:04 +0530

    Runtime: 0 ms (100.00%), Memory: 71.8 MB (53.00%) - Gitcode

--- 53-maximum-subarray/53. Maximum Subarray.cpp
+++ 53-maximum-subarray/53. Maximum Subarray.cpp
@@ -1,15 +1,16 @@
 class Solution{
 public:
     int maxSubArray(vector<int>& nums){
-        int sum=0;
-        int maxi=nums[0];
+        int currentsum=0;
+        int maxsum=nums[0];
         for(int i=0;i<nums.size();i++){
-            sum=sum+nums[i];
-            maxi=max(maxi,sum);
-            if(sum<0){
-            sum=0;
+            currentsum=currentsum+nums[i];
+            maxsum=max(maxsum,currentsum);
+            if(currentsum<0){
+                currentsum=0;
+            }
         }
-        }
-        return maxi;
+        return maxsum;
+        
     }
 };
