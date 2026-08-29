07c761a68d139f301d17ccd0262a8d7867673f74
Ishant <ishantsharma1101@gmail.com>
2026-08-29 17:48:22 +0530

    Runtime: 0 ms (100.00%), Memory: 13.5 MB (41.46%) - Gitcode

--- 206-reverse-linked-list/206. Reverse Linked List.cpp
+++ 206-reverse-linked-list/206. Reverse Linked List.cpp
@@ -1,15 +1,14 @@
 class Solution{
 public:
     ListNode* reverseList(ListNode* head){
-    ListNode*prev=nullptr;
-    ListNode*curr=head;
-    while(curr!=nullptr){
-        ListNode*nextNode=curr->next;
-        curr->next=prev;
-        prev=curr;
-        curr=nextNode;
-    }
-    return prev;
-        
+        ListNode*curr=head;
+        ListNode*prev=nullptr;
+        while(curr!=nullptr){
+            ListNode*nextNode=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=nextNode;
+        }
+        return prev;
     }
 };
