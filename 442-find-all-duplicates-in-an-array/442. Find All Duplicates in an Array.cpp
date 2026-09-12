c40b737965eeba9a2e5dd6a1835d66303515e2d8
Ishant <ishantsharma1101@gmail.com>
2026-09-12 23:25:04 +0530

    Runtime: 0 ms (100.00%), Memory: 48.4 MB (82.07%) - Gitcode

--- 442-find-all-duplicates-in-an-array/442. Find All Duplicates in an Array.cpp
+++ 442-find-all-duplicates-in-an-array/442. Find All Duplicates in an Array.cpp
@@ -1,13 +1,14 @@
 class Solution{
 public:
     vector<int> findDuplicates(vector<int>& nums){
-        unordered_map<int,int>mp;
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
-            if(mp.find(nums[i])!=mp.end()){
-                ans.push_back(nums[i]);
+            int x=abs(nums[i]);
+            if(nums[x-1]<0){
+                ans.push_back(x);
+            }else{
+                nums[x-1]*=-1;
             }
-            mp[nums[i]]++;
         }    
         return ans;    
     }
